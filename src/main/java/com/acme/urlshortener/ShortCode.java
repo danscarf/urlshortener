a941e07bebda4b13e4ea9909ebd615ee476f4606
dscarf <dscarf@gmail.com>
2019-01-27 08:01:12 -0800

    ''

--- src/main/java/com/acme/urlshortener/ShortCode.java
+++ src/main/java/com/acme/urlshortener/ShortCode.java
@@ -3,13 +3,19 @@ package com.acme.urlshortener;
 public class ShortCode {
 
 	private final String url;
+	private final String guid;
 		
-	public ShortCode (String url) {
+	public ShortCode (String url, String guid) {
 		this.url = url;
+		this.guid = guid;
 	}
 	
 	
 	public String GetUrl() {
 		return this.url;
+	}
+	
+	public String getGuid() {
+		return this.guid;
 	}
 }
--- src/main/java/com/acme/urlshortener/UrlShortenerController.java
+++ src/main/java/com/acme/urlshortener/UrlShortenerController.java
@@ -7,17 +7,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import java.util.UUID;
+
 @Controller
 public class UrlShortenerController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/GetShortcode")
+	@GetMapping("/getshortcode")
 	@ResponseBody
 	public ShortCode getShortCode(@RequestParam(name="url", required=false, defaultValue="https://localhost") String url){
-		ShortCode sc = new ShortCode(url);
+		
+		String uuid = UUID.randomUUID().toString();
+		ShortCode sc = new ShortCode(url, uuid);
 		return sc;
 	}
 
+	
+	@GetMapping("/setshortcode")
+	@ResponseBody
+	public ShortCode setShortCode(@RequestParam(name="url", required=false, defaultValue="https://localhost") String url){
+		
+		String uuid = UUID.randomUUID().toString();
+		
+		ShortCode sc = new ShortCode(url, uuid);
+		
+		return sc;
+	}
+	
 
 }
